c2ca6032f75b77baef48ccc7dd827b8049082b14
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-08-05 23:34:01 +0800

    将引脚分文件夹

---
+++ include/gpio/pins.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#pragma region PA
+#include <gpio/PA/GpioPinPA0.h>
+#include <gpio/PA/GpioPinPA10.h>
+#include <gpio/PA/GpioPinPA9.h>
+#pragma endregion
+
+#pragma region PB
+#include <gpio/PB/GpioPinPB0.h>
+#include <gpio/PB/GpioPinPB5.h>
+#pragma endregion
+
+#pragma region PD
+#include <gpio/PD/GpioPinPD0.h>
+#include <gpio/PD/GpioPinPD1.h>
+#include <gpio/PD/GpioPinPD4.h>
+#include <gpio/PD/GpioPinPD5.h>
+#pragma endregion
+
+#pragma region PE
+#include <gpio/PE/GpioPinPE3.h>
+#include <gpio/PE/GpioPinPE4.h>
+#include <gpio/PE/GpioPinPE5.h>
+#pragma endregion
+
+#pragma region PG
+#include <gpio/PG/GpioPinPG0.h>
+#include <gpio/PG/GpioPinPG12.h>
+#pragma endregion
