78df5797306eb0e2ae76f42141f43c1dde350eed
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-08-19 23:33:59 +0800

    格式化

--- private_src/di/DICreate_GpioPinOptions.cpp
+++ private_src/di/DICreate_GpioPinOptions.cpp
@@ -1,7 +1,7 @@
-#include <GpioPinOptions.h>
 #include <bsp-interface/di.h>
+#include <GpioPinOptions.h>
 
 std::shared_ptr<bsp::IGpioPinOptions> DICreate_GpioPinOptions()
 {
-	return std::shared_ptr<bsp::IGpioPinOptions>{new bsp::GpioPinOptions{}};
+    return std::shared_ptr<bsp::IGpioPinOptions>{new bsp::GpioPinOptions{}};
 }
